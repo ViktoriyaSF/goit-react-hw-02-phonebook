4d109a4618b373e84a0144b818b1a233e76fbf3e
ViktoriyaSF <kievstar01051985@gmail.com>
2023-04-23 22:17:43 +0300

    part 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { GlobalStyle } from './BasicStyles/GlobalStyle';
+import { Layout } from './Layout/Layout';
+import { nanoid } from 'nanoid';
+
+import { ContactForm } from './ContactForm/ContactForm';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  //створюємо метод для додавання контактів в стейт
+  addContact = (name, number) => {
+    const { contacts } = this.state;
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+  };
+
+  render() {
+    return (
+      <Layout>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+        <h2>Contacts</h2>
+        {/* <Filter ... /> */}
+        {/* <ContactList ... /> */}
+
+        <GlobalStyle />
+      </Layout>
+    );
+  }
+}
